b4070129dbf4a05122cecf050b06ccd021328359
Andreas Longva <andreas.b.longva@gmail.com>
2015-05-22 23:05:56 -0700

    Added super-basic console interface

--- src/main/scala/ConsoleApp.scala
+++ src/main/scala/ConsoleApp.scala
@@ -1,5 +1,17 @@
 package simplesymbols
+import Parser._
 
 object ConsoleApp extends App {
     println("Starting Simple Symbols...")
+
+    val env = new Environment(Map("x" -> 3.0, "y" -> 5.0))
+
+    for (line <- io.Source.stdin.getLines()) {
+        try {
+            val func = parse(line)
+            println("ans = " + func.eval(env))
+        } catch {
+            case e: Throwable => println("Illegal input: " + e.toString)
+        }
+    }
 }
